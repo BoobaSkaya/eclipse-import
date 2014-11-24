019bfebbd355f07a67f365fb11cb37572ff017a3
Booba Skaya <booba.skaya@gmail.com>
2014-11-24 08:08:51 +0100

    Create dummy example application with one toolbar button

---
+++ com.eclipse.project.tools.ui/src/handler/ImportProjectHandler.java
@@ -0,0 +1,12 @@
+ 
+package handler;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class ImportProjectHandler {
+	@Execute
+	public void execute() {
+		System.out.println("Hello from the code");
+	}
+		
+}
